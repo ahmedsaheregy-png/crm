075cacdbee0c13f1bf8d4e7553fe2c99511f7b36
Your Name <your@example.com>
2025-11-24 00:58:49 +0300

    Fixing RTF format and testing simple deployment

--- app.py
+++ app.py
@@ -1,70 +1,11 @@
-{\rtf1}import os
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
-# تهيئة قاعدة البيانات باستخدام متغير البيئة DATABASE_URL
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') 
-db = SQLAlchemy(app)
 
-# =========================================================
-# الكود الحاسم: سيُنشئ الجدول لمرة واحدة عند تشغيل التطبيق
-# =========================================================
-with app.app_context():
-    db.create_all()
-# =========================================================
-
-class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-    
-    def __repr__(self):
-        return '<Task %r>' % self.id
-
-@app.route('/', methods=['POST', 'GET'])
+@app.route("/")
 def index():
-    if request.method == 'POST':
-        task_content = request.form['content']
-        new_task = Task(content=task_content)
-
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue adding your task'
-
-    else:
-        tasks = Task.query.order_by(Task.date_created).all()
-        return render_template('index.html', tasks=tasks)
-
-@app.route('/delete/<int:id>')
-def delete(id):
-    task_to_delete = Task.query.get_or_404(id)
-
-    try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
-        return redirect('/')
-    except:
-        return 'There was a problem deleting that task'
-
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
-    task = Task.query.get_or_404(id)
-
-    if request.method == 'POST':
-        task.content = request.form['content']
-
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue updating your task'
-    else:
-        return render_template('update.html', task=task)
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
